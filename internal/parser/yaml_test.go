fff2024b1fa9f83d2ca792025fc87ae6043a7ae5
Stephen Robinson <s.j.robinson1@lancaster.ac.uk>
2021-02-23 11:49:04 +0000

    Add tests to cover complete workflow definition

--- internal/parser/yaml_test.go
+++ internal/parser/yaml_test.go
@@ -62,5 +62,102 @@ var _ = Describe("Yaml File Parsing", func() {
 				Expect(wf).ToNot(BeNil())
 			})
 		})
+		Context("Workflow Definition", func() {
+			It("has correct workflow ID", func() {
+				Expect(wf.ID).To(Equal("lulibrary.workflows.reloadAllUsers"))
+			})
+			It("has correct workflow name", func() {
+				Expect(wf.Name).To(Equal("Reload All Users"))
+			})
+		})
+		Context("Workflow Triggers", func() {
+			It("has workflow triggers defined", func() {
+				Expect(wf.Triggers).ToNot(BeZero())
+			})
+			It("has correct scheduled trigger", func() {
+				Expect(wf.Triggers.Schedule).To(Equal("0/1 * * * *"))
+			})
+			It("has correct manual trigger", func() {
+				Expect(wf.Triggers.Manual).To(BeFalse())
+			})
+			It("has correct workflows in workflow triggers", func() {
+				Expect(wf.Triggers.Workflows).To(ContainElement("lulibrary.workflows.fullReload"))
+			})
+		})
+		Context("Workflow Arguments", func() {
+			It("has workflow arguments defined", func() {
+				Expect(wf.Args).ToNot(BeZero())
+			})
+			It("has correct workflow argument", func() {
+				Expect(wf.Args).To(HaveKey("a1"))
+				Expect(wf.Args["a1"]).To(Equal("Argument1"))
+			})
+		})
+		Context("Workflow Parameters", func() {
+			It("has workflow parameters defined", func() {
+				Expect(wf.Params).ToNot(BeZero())
+			})
+			It("has correct parameter definitions", func() {
+				p1 := WorkflowParameter{
+					Name: "p1",
+					Type: "string",
+				}
+				p2 := WorkflowParameter{
+					Name: "p2",
+					Type: "boolean",
+				}
+				Expect(wf.Params).To(ContainElements([]WorkflowParameter{p1, p2}))
+			})
+		})
+		Context("Workflow Tasks", func() {
+			It("has workflow tasks defined", func() {
+				Expect(wf.Tasks).ToNot(BeZero())
+			})
+			It("has correct task definition for publish initial message", func() {
+				Expect(wf.Tasks).To(HaveKey("publish_initial_message"))
+				task := wf.Tasks["publish_initial_message"]
+				Expect(task.Name).To(Equal("Publish get all users"))
+				Expect(task.Uses).To(Equal("Hopscotch/PublishMessage@v1"))
+				Expect(task.Config).ToNot(BeZero())
+				Expect(task.Args).To(BeZero())
+			})
+			It("has correct task config for publish initial message", func() {
+				task := wf.Tasks["publish_initial_message"]
+				Expect(task.Config).ToNot(BeZero())
+				Expect(task.Config).To(HaveKey("type"))
+				Expect(task.Config).To(HaveKey("version"))
+				Expect(task.Config).To(HaveKey("data_template"))
+			})
+			It("has correct task definition for get all users", func() {
+				Expect(wf.Tasks).To(HaveKey("get_all_users"))
+				task := wf.Tasks["get_all_users"]
+				Expect(task.Name).To(Equal("Get All Users"))
+				Expect(task.Uses).To(Equal("Hopscotch/UseJob@v1"))
+				Expect(task.Config).ToNot(BeZero())
+				Expect(task.Args).ToNot(BeZero())
+			})
+			It("has correct task config for get all users", func() {
+				task := wf.Tasks["get_all_users"]
+				Expect(task.Config).ToNot(BeZero())
+				Expect(task.Config).To(HaveKey("job"))
+			})
+			It("has correct task arguments for get all users", func() {
+				task := wf.Tasks["get_all_users"]
+				Expect(task.Args).ToNot(BeZero())
+				Expect(task.Args).To(HaveKey("hello"))
+			})
+		})
+		Context("Workflow Notifications", func() {
+			It("has workflow notifications defined", func() {
+				Expect(wf.Notifications).ToNot(BeZero())
+			})
+			It("has correct workflow notification definition for onSuccess", func() {
+				Expect(wf.Notifications).To(HaveKey("onSuccess"))
+				Expect(wf.Notifications["onSuccess"]).ToNot(BeZero())
+				Expect(wf.Notifications["onSuccess"].User).To(BeTrue())
+				Expect(wf.Notifications["onSuccess"].Schedule).To(BeZero())
+				Expect(wf.Notifications["onSuccess"].Workflow).To(BeZero())
+			})
+		})
 	})
 })
