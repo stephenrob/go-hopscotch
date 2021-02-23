0cd76e7bb0a008f6d509a079cdf50394cb795d5e
Stephen Robinson <s.j.robinson1@lancaster.ac.uk>
2021-02-23 11:28:01 +0000

    Add all workflow component structs

--- internal/parser/example_yaml_test.go
+++ internal/parser/example_yaml_test.go
@@ -9,10 +9,10 @@ func ExampleParseYamlWorkflow() {
 
 	wf, _ := ParseYamlWorkflow(inputFile)
 
-	fmt.Printf("%+v\n", wf)
+	fmt.Printf("%#v", wf)
 
 	// Output:
-	// &{ID:lulibrary.workflows.reloadAllUsers Name:Reload All Users}
+	// &parser.Workflow{ID:"lulibrary.workflows.reloadAllUsers", Name:"Reload All Users", Triggers:parser.WorkflowTriggers{Schedule:"0/1 * * * * ", Manual:true, Workflows:[]string{"lulibrary.workflows.fullReload"}}, Args:map[string]string{"a1":"Argument1"}, Params:[]parser.WorkflowParameter{parser.WorkflowParameter{Name:"p1", Type:"string"}, parser.WorkflowParameter{Name:"p2", Type:"boolean"}}, Tasks:map[string]parser.WorkflowTask{"get_all_users":parser.WorkflowTask{Name:"Get All Users", Uses:"Hopscotch/UseJob@v1", Config:map[string]string{"job":"UserLoader::Jobs::GetAllUserCids"}, Args:map[string]string{"hello":"World"}}, "publish_initial_message":parser.WorkflowTask{Name:"Publish get all users", Uses:"Hopscotch/PublishMessage@v1", Config:map[string]string{"data_template":"{\n  \"hello\": \"{{.Args.a1}}\"\n}\n", "type":"userLoader.ldap.getAllUserCids", "version":"0.1.0"}, Args:map[string]string(nil)}, "reload_all_users":parser.WorkflowTask{Name:"Reload All Users", Uses:"Hopscotch/UseJob@v1", Config:map[string]string{"job":"UserLoader::Jobs::ReloadAllUserCids"}, Args:map[string]string(nil)}}, Notifications:map[string]parser.WorkflowNotification{"onSuccess":parser.WorkflowNotification{User:true, Workflow:map[string]bool(nil), Schedule:false}}}
 }
 
 // Parses a workflow where there is no file present at the filepath and returns error
